66ea2c0c9618c42271d46668a711686d42c39d69
AmitaiB <blickstein@gmail.com>
2015-06-01 13:42:25 -0400

    reboot - adds main menu

---
+++ deployOnDayOne/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  deployOnDayOne
+//
+//  Created by Amitai Blickstein on 6/1/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ deployOnDayOne/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  deployOnDayOne
+//
+//  Created by Amitai Blickstein on 6/1/15.
+//  Copyright (c) 2015 Zachary Drossman. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
